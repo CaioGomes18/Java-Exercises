5b64080eb0eb6ffa22918f69f8143f3f1e5b99dc
CaioGomes18 <136707915+CaioGomes18@users.noreply.github.com>
2023-10-04 20:26:47 -0300

    fazendo alterações

--- Fisrt/src/App.java
+++ Fisrt/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("olá!");
+
+        int v = 1;
+
+        if(v == 1)
+        {
+            System.out.println("Meu pai");
+        }
     }
+
+    
+
 }
