c4d54c6d239f56a6629502f61f8e4b01c84b6e1c
CaioGomes18 <136707915+CaioGomes18@users.noreply.github.com>
2023-11-08 01:18:28 -0300

    condicionais

--- Estruturas-Condicionais/src/edu/caio/condicionais/CondicoesEncadeadas.java
+++ Estruturas-Condicionais/src/edu/caio/condicionais/CondicoesEncadeadas.java
@@ -1,16 +1,16 @@
 package edu.caio.condicionais;
-public class CondicoesEncadeadas {
-    // ResultadoEscolar.java
 
+public class CondicoesEncadeadas {
+    
     public static void main(String[] args) {
         int nota = 6;
 
-	if (nota >= 7)
-		System.out.println("Aprovado");
-	else if (nota >= 5 && nota < 7)
-		System.out.println("Recuperação");
-	else
-		System.out.println("Reprovado");
+		if (nota >= 7)
+			System.out.println("Aprovado");
+		else if (nota >= 5 && nota < 7)
+			System.out.println("Recuperação");
+		else
+			System.out.println("Reprovado");
     }
 }
 
