fcba83c4754166109cebccdfa705f89109b0d123
CaioGomes18 <136707915+CaioGomes18@users.noreply.github.com>
2023-11-08 23:34:58 -0300

    condicinais ternarias

---
+++ Estruturas-Condicionais/src/edu/caio/condicionais/CondicionalTernaria.java
@@ -0,0 +1,19 @@
+package edu.caio.condicionais;
+
+
+public class CondicionalTernaria {
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+
+      
+        
+		/* int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado); */
+	}
+}
+
+    
